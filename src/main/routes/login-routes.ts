2af74bf1946f2e41d6635d3c20f7ecaa3331cd7b
Silas <silas.paixao873@gmail.com>
2023-11-03 05:21:10 -0300

    docs: update api docs

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { makeSignUpController } from '@/main/factories/controllers/login/signup/signup-controller-factory'
+import { makeLoginController } from '@/main/factories/controllers/login/login/login-controller-factory'
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup-factory'
-import { makeLoginController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
